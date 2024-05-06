d3c5f5e5d0660a1afdb38b9e974b6e29c553de13
Jiangjin Wang <kaymw@aosc.io>
2024-05-06 00:06:32 -0700

    fix(settings): use system fonts by default

--- Telegram/SourceFiles/core/core_settings.h
+++ Telegram/SourceFiles/core/core_settings.h
@@ -12,6 +12,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "dialogs/ui/dialogs_quick_action.h"
 #include "window/themes/window_themes_embedded.h"
 #include "ui/chat/attach/attach_send_files_way.h"
+#include "ui/style/style_core_font.h"
 #include "base/flags.h"
 #include "emoji.h"
 
@@ -1177,7 +1178,7 @@ private:
 	rpl::variable<bool> _ttlVoiceClickTooltipHidden = false;
 	WindowPosition _ivPosition;
 	WindowPosition _callPanelPosition;
-	QString _customFontFamily;
+	QString _customFontFamily = style::SystemFontTag();
 	bool _systemUnlockEnabled = false;
 	std::optional<bool> _weatherInCelsius;
 	QByteArray _tonsiteStorageToken;
